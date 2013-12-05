bac7c654a21881aa201293d48e0b90a861970bef
Joshua Phenicie <joshua@nerdybytes.com>
2013-12-04 22:36:12 -0800

    First Commit
    This is the first commit
    
    1
    2
    3
    4
    =========
    
    is this a read me file that they speak of
    
    ========
    
    Thaaaaanks

---
+++ tuuuts.js
@@ -0,0 +1,26 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+if (true) {};
+
+<?php  ?>
+
+<?php echo htmlentities($var, ENT_QUOTES, 'utf-8') ?>
+
+f
+
+#!/usr/bin/env 
+
+if
+
+	if (true) {};
+
+if (true) {} else{};
+
+Hello, this is a snippet.
+
+Hello, this is a snippet.
